efc66bc0f8e4827661d7bfbe9f5f524bb6406e76
Igor Grigorev <io.grigorev@physics.msu.ru>
2017-02-09 21:09:36 +0300

    task-manager-0.0.1
    simple task manager

---
+++ app/components/TodoList.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var TodoListItem = require('./TodoListItem');
+
+var TodoList = React.createClass({
+	render: function() {
+		var createItem = function(itemText) {
+			return (
+				<TodoListItem>{itemText}</TodoListItem>
+			);
+		};
+		return <ul>{this.props.items.map(createItem)}</ul>;
+	}
+});
+
+module.exports = TodoList;
